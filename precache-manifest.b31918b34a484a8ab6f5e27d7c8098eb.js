adee5fab1cc26ea8b7ef03a8b5dc9e6167c7fd59
Your Name <you@example.com>
2020-05-31 20:00:16 +0545

    Updates

---
+++ precache-manifest.b31918b34a484a8ab6f5e27d7c8098eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71a5d0ed06d98dcef8c1b5d33e6e9dd",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/index.html"
+  },
+  {
+    "revision": "9de4fadbb8fa000d1648",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/static/css/main.6e28284c.chunk.css"
+  },
+  {
+    "revision": "7e1d43c1b64b66e87bc8",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/static/js/2.7d1b9fa8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/static/js/2.7d1b9fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de4fadbb8fa000d1648",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/static/js/main.1db1387d.chunk.js"
+  },
+  {
+    "revision": "c6f109f79fad3f160b78",
+    "url": "/Complete-Anime-Quiz-game-ReactJs/static/js/runtime-main.852ae9e7.js"
+  }
+]);
